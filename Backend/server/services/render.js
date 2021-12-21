8a7782cecd5ebcccaa0612fa1c80410aff5d12af
XwyzWorm <pratamaazmi1@gmail.com>
2021-12-21 11:30:08 +0700

    Configuring the Routes

---
+++ Backend/server/services/render.js
@@ -0,0 +1,9 @@
+exports.home = (req, res) => {
+  return res.render("index");
+};
+exports.add_user = (req, res) => {
+  return res.render("add_user");
+};
+exports.update_user = (req, res) => {
+  return res.render("update_user");
+};
